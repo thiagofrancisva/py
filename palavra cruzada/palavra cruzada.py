227588dfd3b0485359942f8a5bfc5640e0c00792
thiagofrancisva <thiagofrancis93@gmail.com>
2024-07-06 11:50:38 -0300

    alternativa ao slice

--- palavra cruzada/palavra cruzada.py
+++ palavra cruzada/palavra cruzada.py
@@ -5,6 +5,7 @@ palavra_copia = palavra.copy()
 
 leitura = ["v","h","v","h","v"]
 palavra_leitura = []
+indice_ref = []
 
 palavra_escrita = []
 letra_escrita = []
@@ -13,6 +14,10 @@ letra_cruzada = []
 quadro_invalido = []
 quadro_invalido_v = []
 quadro_invalido_h = []
+
+def copiar(i,lista,ref,palavra):
+    for x in range(len(palavra)):
+        letra_quadro_analise.append(lista[ref[i]+x])
 
 for x in range(8):
     if len(palavra_copia) == len(palavra):
@@ -31,6 +36,7 @@ for x in range(8):
             
             quadro_invalido.append(quadro_inicial - 100)
             quadro_invalido.append(quadro_inicial + (len(letra_escrita) - 1) * 100)
+            
             w = 0
 
             while w < len(letra_escrita):
@@ -57,5 +63,35 @@ for x in range(8):
                 quadro_inicial += 1
 
                 w += 1
+
+        indice_ref.append(0)
+    
+    else:
+        indice_palavra_copia = []
+        indice_palavra_escrita = []
+        letra_cruzavel_quadro = []
+        letra_cruzavel_qtd = []
+        letra_quadro_copia = letra_quadro.copy()
+        letra_indice = []
+
+        for s in range(len(palavra_escrita)):
+            
+            palavra_analise = palavra_escrita[s].split()
+            w = 0
+            letra_quadro_analise = []
+            copiar(s,letra_quadro_copia,indice_ref,palavra_escrita)
+
+            #while w < len(palavra_copia):
+
+                #v = 0
                 
+
+                
+                    
+                    #if palavra_leitura[s] == "v":
+
+                    #elif palavra_escrita[s] == "h":
+                #w += 1
+            
+        
 print(letra_quadro)
